62d5924047ec77a0ea33c56b0ccee350dbe0becb
Kirstin <kpetersen@elon.edu>
2023-05-31 17:34:52 +0100

    add icons and links to contact section + style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,25 @@ const Contact = () => {
       id="contact"
     >
       <h2>Contact</h2>
+      <div className="skills-icon-container contact-container">
+        <a
+          href="https://github.com/kpetersen04"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className=" _icon devicon-github-original"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kirstin-petersen/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="_icon devicon-linkedin-plain"></i>
+        </a>
+        <a href="mailto:kpetersen@elon.edu">
+          <i className="_icon _email bx bx-envelope" type="solid"></i>
+        </a>
+      </div>
     </section>
   );
 };
