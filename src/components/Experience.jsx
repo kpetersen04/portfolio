ba2b1d1abd2121d725a5c0e5b40b4aef9055ef48
Kirstin <kpetersen@elon.edu>
2023-05-31 16:48:46 +0100

    update experience section + styled

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,30 +3,51 @@ const Experience = () => {
     <section className="experience-container section" id="experience">
       <h2>Experience</h2>
       <div className="job-container">
-        <p className="_business">General Assembly</p>
+        <div className="experience-heading-container">
+          <img src="https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png" />
+          <h3 className="_business">General Assembly</h3>
+        </div>
         <p className="_job-name">Software Engineering Immersive</p>
         <p className="_date">November 2022 - May 2023</p>
         <p className="_description">
           Part-time software engineering immersive course held over Zoom and
           Slack on Tuesday and Thursday nights from 18:00 to 22:00 and Saturdays
-          from 10:00 to 17:00. Throughout the course we had regular stand-ups
-          and pair programming opportunities and I completed four main projects.
+          from 10:00 to 17:00.{" "}
+        </p>
+        <p className="_description">
+          Throughout the course we had regular stand-ups and pair programming
+          opportunities and I completed four main projects.
         </p>
       </div>
       <div className="job-container">
-        <h3>Wise</h3>
-        <p className="_job-name">WiseWomenCode Spring Insights Day</p>
+        <div className="experience-heading-container">
+          <img src="./src/assets/images/wiseLogo.png" />
+          <h3 className="_business">Wise</h3>
+        </div>
+        <p className="_job-name">WiseWomenCode</p>
         <p className="_date">27 March 2023 - 29 March 2023</p>
         <p className="_description">
-          Shadowed a Wise full-stack Software Engineer for a day Gained insight
-          into Product Engineering, Security and Machine Learning Participated
-          in a coding workshop working with APIs to create quotes, recipients
-          and transfers for payment
+          Selected from 100+ applicants to take part in the 3-day WiseWomenCode
+          insight programme at the Wise London office.{" "}
+        </p>
+        <p className="_description">
+          I attended lectures on Machine Learning, Product Engineering and
+          Security, along with coding workshops using APIs to create quotes,
+          recipients and transfers for payment.{" "}
+        </p>
+        <p className="_description">
+          I also shadowed a full-stack Wise Software Engineer which included
+          attending team planning meetings, completing a ticket to update
+          content on the Wise website and participating in a pair programming
+          session.
         </p>
       </div>
       <div className="job-container">
-        <h3>University of London</h3>
-        <p className="_job-name">Laws Manager (Student Support and Events)</p>
+        <div className="experience-heading-container">
+          <img src="./src/assets/images/uolLogo.png" />
+          <h3 className="_business">University of London</h3>
+        </div>
+        <p className="_job-name">Laws Manager</p>
         <p className="_date">December 2018 - June 2023</p>
         <p className="_description">
           Student relationship professional responsible for the delivery of key
